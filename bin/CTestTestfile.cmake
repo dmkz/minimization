e092a695775be3670b66771264818c5eba3ff920
Zolkin Artem <arquestro93@gmail.com>
2017-12-24 16:19:42 +0300

    Auto stash before merge of "develop" and "origin/develop"

---
+++ bin/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/Programming Projects/minimization
+# Build directory: D:/Programming Projects/minimization/bin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Factorial "Test")
+subdirs("tests")
---
+++ bin/tests/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/test.cpp.obj"
+  "CMakeFiles/tests.dir/__/src/sobolseqgenerator.cpp.obj"
+  "tests.pdb"
+  "tests.exe"
+  "tests.exe.manifest"
+  "libtests.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
