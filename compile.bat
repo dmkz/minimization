116d0a88af9862672333a5eb98793c189cf0c14f
Dmitry Kozyrev <dmkozyrev@rambler.ru>
2017-12-26 01:05:46 +0300

    Кодировка и скрипт компиляции под винду

--- compile.bat
+++ compile.bat
@@ -1,10 +1,7 @@
-@echo off
-PATH | findstr /I GnuWin32\bin >nul &&echo Check PATH to make.exe: OK ||echo Check path to make.exe: ERROR
-PATH | findstr /I CMake\bin >nul &&echo Check PATH to cmake.exe: OK ||echo Check PATH to cmake.exe: ERROR
-PATH | findstr /I MinGW\bin >nul &&echo Check PATH to MinGW compiler: OK||echo Check PATH to MinGW compiler: ERROR
-
 @echo on
+cd build
 del main.exe
-cmake -G "MinGW Makefiles"
+cmake -DCMAKE_BUILD_TYPE=Debug -G "MSYS Makefiles" .. && cmake --build . --config Debug -- -j6
 make
 main
+cd ..
