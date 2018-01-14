e51b486d3318c26b7cfe6560ea3f801533b434ea
Allentiy <a-burahanova@yandex.ru>
2018-01-14 17:57:04 +0300

    Небольшие изменения

--- tools/script2.py
+++ tools/script2.py
@@ -1,23 +1,46 @@
 import matplotlib.pyplot as plt 
 import fileinput 
+import re
+import os
 
-f = open("testn.txt", 'r') 
-x = [] 
-y = [] 
-line = f.readline() 
-while line: 
-	a, b = line.split(" ") 
-	x.append(a) 
-	y.append(b) 
-	line = f.readline() 
+def rf(f):
+	x = []
+	y = []    
+	line = f.readline()
+	while line:
+		a, b = line.split(" ")
+		x.append(a)
+		y.append(b)
+		line = f.readline()
 
-plt.plot(x,y, 'rs', markersize = 1) 
+	return x,y
 
-f.close() 
+def find_files(path, regex):
+    ret = []
+    pattern = re.compile(regex)
+    for root, dir, files in os.walk(path):
+        for name in files:
+            if pattern.match(name):
+                ret.append(name)
+    return ret
 
-plt.xlim(0,1) 
-plt.ylim(0,1) 
-plt.xlabel('x') 
-plt.ylabel('y')
-plt.show() 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+file_list = find_files(dir_path, ".*projection_")
+print(file_list)
+
+
+
+for file in file_list:
+	f = open(file, 'r')
+	x, y = rf(f)
+	plt.xlim(0,1) 
+	plt.ylim(0,1) 
+	plt.xlabel('x') 
+	plt.ylabel('y')
+	plt.plot(x,y, 'rs', markersize = 1)
+	plt.savefig(file+'.png')
+	plt.clf()
+	f.close()
+
+
 
