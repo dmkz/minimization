991421916affabd03839573367b3dfd59df889f6
Dmitry <dmkozyrev@rambler.ru>
2018-01-15 15:46:53 +0300

    Revert "Добавлено условие остановки"
    This reverts commit 4e0fa343e28a07c35565f19879320d4496511dd6.

--- src/nesterov.cpp
+++ src/nesterov.cpp
@@ -1,8 +1,8 @@
 #include "nesterov.hpp"
-#include "stopCondition.h"
 
-std::pair<Vector, int> nesterov(Function f, Vector startingPoint, stopCondition condition) {
-	ld ro = 2.0;
+// Метод Нестерова (возвращается результат - точка минимума и количество сделанных итераций)
+std::pair<Vector, int> nesterov(Function f, Vector startingPoint, int iter_limit) {
+    ld ro = 2.0;
 	ld teta = 1.1;
 	ld alfa = 1.0;
 
@@ -13,16 +13,13 @@ std::pair<Vector, int> nesterov(Function f, Vector startingPoint, stopCondition 
 	ld alfaNext = 0.0;
 	ld A = 0;
 
-	Vector xPrev = startingPoint; //Переменная для хранения x(i-1)
+    Vector xPrev = startingPoint; //Переменная для хранения x(i-1)
 	ld fPrev = 0; //Переменная для хранения f(x(i-1))
 	ld fCurrent = 0; //Перменная для хранения f(x(i))
 	Vector xCurrent = startingPoint;  //Переменная для хранения x(i)
-
-	int iterations = 0;
-
-	condition.update(xPrev, xCurrent, fPrev, fCurrent, iterations);
-
-	while (!condition.stopConditionHappened()) {
+    
+    int iterations = 0; //Переменная для хранения номера итерации
+	while (iterations < iter_limit) {
 		while (true) {
 			alfaNext = alfa + std::sqrt(alfa*alfa + 2 * alfa*A);
 			y = (A / (A + alfaNext)) * x + (alfaNext / (A + alfaNext)) * v;
@@ -36,15 +33,13 @@ std::pair<Vector, int> nesterov(Function f, Vector startingPoint, stopCondition 
 		v = v - alfaNext * (grad(f, xNext));
 		A += alfaNext;
 		alfaNext = teta * alfa;
-		++iterations;
-
-		//Запись значений x(i). x(i-1), f(x(i)), f(x(i-1))
+        ++iterations;
+        
+        //Запись значений x(i). x(i-1), f(x(i)), f(x(i-1))
 		xPrev = xCurrent;
 		fPrev = fCurrent;
 		xCurrent = xNext;
 		fCurrent = f(xCurrent);
-
-		condition.update(xPrev, xCurrent, fPrev, fCurrent, iterations);
 	}
-	return{ xNext, iterations };
+	return {xNext, iterations};
 }
