1e53ad4594ca602b3602a01a43f448ff08f38f33
Dmitry <dmkozyrev@rambler.ru>
2018-01-15 15:47:01 +0300

    Revert "Update nesterov.hpp"
    This reverts commit 63b49f629d601a1fe7d437224f9b0da6348d1f14.

--- include/nesterov.hpp
+++ include/nesterov.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "math.hpp"
-#include "stopCondition.h"
 
 // Метод Нестерова (возвращается результат - точка минимума и количество сделанных итераций)
-std::pair<Vector, int> nesterov(Function f, Vector startingPoint, stopCondition condition);
+std::pair<Vector, int> nesterov(Function f, Vector startingPoint, int iter_limit = 100);
